74219d4602bf8d5da7c40603fdf6d61a21ed005a
danielemurgioni <dashomew@gmail.com>
2025-05-15 17:44:36 +0200

    [UPD] render the endpoint by using axios

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,11 +1,38 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+
+const prodEndpoint = "https://fakestoreapi.com/products"
 
 const Products = () => {
+
+    const [products, setProducts] = useState([])
+
+    const fetchProducts = () => {
+        axios.get(prodEndpoint).then((resp) => { setProducts(resp.data) })
+    }
+
+    useEffect(() => {
+        fetchProducts()
+    }, [])
+
     return (
         <>
-            <Navbar />
-            <h1>Products</h1>
+            <div className="navbar-container">
+                <Navbar />
+            </div>
+            <div className="products-container">
+                <h1>Products</h1>
+                {products.map((item) => (
+                    <div key={item.id} className='product'>
+                        <h2>{item.title}</h2>
+                        <img src={item.image} alt="" />
+                        <p>{item.description}</p>
+                        <p>Price: {item.price}£</p>
+                    </div>
+                ))}
+            </div>
         </>
     )
 }

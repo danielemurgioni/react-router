4902565c10fe941ae4ed0d7607cabb70212c3614
danielemurgioni <dashomew@gmail.com>
2025-05-15 17:00:04 +0200

    [UPD] import the pages and install react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+/*pages*/
+import Homepage from './pages/Homepage'
+import Products from './pages/Products'
+import AboutUs from './pages/AboutUs'
 
 function App() {
 
   return (
     <>
-      <h1>React Router</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/about-us" element={<AboutUs />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
